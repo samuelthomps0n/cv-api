e58ce64d88efda874cc5a879a4f8b418b129099b
Sam Thompson <sam@samuelthompson.co.uk>
2018-06-24 13:17:29 +0100

    Comment out POST routes
    Stop people being able to add to the Database by calling the API

--- app/routes/cv_routes.js
+++ app/routes/cv_routes.js
@@ -1,23 +1,23 @@
 module.exports = function(app, db) {
-	app.post('/experience', (req, res) => {
+	// app.post('/experience', (req, res) => {
 		
-		const job = {
-			title: req.body.title,
-			location: req.body.location,
-			company: req.body.company,
-			description: req.body.description,
-			start_date: req.body.start_date,
-			end_date: req.body.end_date
-		}
+	// 	const job = {
+	// 		title: req.body.title,
+	// 		location: req.body.location,
+	// 		company: req.body.company,
+	// 		description: req.body.description,
+	// 		start_date: req.body.start_date,
+	// 		end_date: req.body.end_date
+	// 	}
 
-		db.collection('jobs').insert(job, (err, result) => {
-			if(err) {
-				res.send({ 'error': 'An error has ocurred' });
-			} else {
-				res.send(result.ops[0]);
-			}
-		})
-	})
+	// 	db.collection('jobs').insert(job, (err, result) => {
+	// 		if(err) {
+	// 			res.send({ 'error': 'An error has ocurred' });
+	// 		} else {
+	// 			res.send(result.ops[0]);
+	// 		}
+	// 	})
+	// })
 
 	app.get('/experience', (req, res) => {
 		db.collection('jobs').find({}).sort( {_id: -1} ).toArray((err, results) => {
@@ -29,19 +29,19 @@ module.exports = function(app, db) {
 		});
 	})
 
-	app.post('/skill', (req, res) => {
-		const skill = {
-			name: req.body.name
-		}
+	// app.post('/skill', (req, res) => {
+	// 	const skill = {
+	// 		name: req.body.name
+	// 	}
 
-		db.collection('skills').insert(skill, (err, result) => {
-			if(err) {
-				res.send({ 'error': 'An error has occured'});
-			} else {
-				res.send(result.ops[0]);
-			}
-		});
-	})
+	// 	db.collection('skills').insert(skill, (err, result) => {
+	// 		if(err) {
+	// 			res.send({ 'error': 'An error has occured'});
+	// 		} else {
+	// 			res.send(result.ops[0]);
+	// 		}
+	// 	});
+	// })
 
 	app.get('/skills', (req, res) => {
 		db.collection('skills').find({}).toArray((err, results) => {
@@ -53,20 +53,20 @@ module.exports = function(app, db) {
 		});
 	})
 
-	app.post('/contact', (req, res) => {
-		const contact = {
-			type: req.body.type,
-			value: req.body.value
-		};
+	// app.post('/contact', (req, res) => {
+	// 	const contact = {
+	// 		type: req.body.type,
+	// 		value: req.body.value
+	// 	};
 
-		db.collection('contact').insert(contact, (err, result) => {
-			if(err) {
-				res.send(err);
-			} else {
-				res.send(result.ops[0]);
-			}
-		});
-	})
+	// 	db.collection('contact').insert(contact, (err, result) => {
+	// 		if(err) {
+	// 			res.send(err);
+	// 		} else {
+	// 			res.send(result.ops[0]);
+	// 		}
+	// 	});
+	// })
 
 	app.get('/contact', (req, res) => {
 		db.collection('contact').find({}).toArray((err, results) => {
